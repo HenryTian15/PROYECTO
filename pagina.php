d2e949f6d1fff59cf08b818ce5814dd5acfb4099
PaolaCaillagua <98109071+PaolaCaillagua@users.noreply.github.com>
2022-01-24 19:15:49 -0500

    Add files via upload

---
+++ pagina.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet"type="text/css" href="estilos.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,400&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header" id="inicio">
+        <img src="menus1.png" alt="" class="hamburger">
+        <nav class="menu-navegacion">
+            <a href="#inicio">Inicio</a>
+            <a href="#servicios">Servivios</a>
+            <a href="#galeria">Galeria</a>
+            <a href="#registrese">Registrese</a>
+            <a href="#contactos">Contactos</a>
+        </nav>
+        <div class="contenedor head">
+              <h1 class="titulo">Patitas y Bigotes</h1>
+              <p class="copy">No compres adopta un amigo</p>
+            </div>
+       </div>
+    </header>
+    <main>
+        <section class="contenedor" id=servicios>
+            <h2 class="subtitulo">Nuestros servicios</h2>
+            <div class="contenedor-servicios">
+               <img src="gatoperro.png" alt=""> 
+               <div class="checklist-servicio">
+                   <div class="service">
+                       <h3 class="n-service"><span class="number"></span> Los animales me importan</h3>
+                       <p>A veces llegan sin avisar, otras somos nosotros quienes las buscamos. De cualquier manera, siempre es generoso el hecho de abrir el corazón para darle hogar a una mascota. Y ellas siempre saben como recompensarnos.<p>
+                   </div>
+               </div>
+            </div>
+
+        </section>
+        <section class="gallery" id="galeria">
+            <div class="contenedor">
+               <h2 class="subtitulo">Galeria </h2> 
+               <div class="contenedor-galeria">
+                   <img src="imagen1.jpg" alt="" class="img-galeria">
+                   <img src="imagen2.jpg" alt="" class="img-galeria">
+                   <img src="imagen3.jpg" alt="" class="img-galeria">
+                   <img src="imagen4.jpg" alt=""
+                   class="img-galeria">
+                   <img src="imagen5.jpg" alt="" class="img-galeria">
+                   <img src="imagen6.jpg" alt="" class="img-galeria">
+                   <img src="imagen7.jpg" alt="" class="img-galeria">
+                   <img src="imagen8.jpg" alt="" class="img-galeria">
+                   <img src="imagen9.jpg" alt="" class="img-galeria">
+                   <img src="imagen10.jpg" alt=""
+                   class="img-galeria">
+                   <img src="imagen11.jpg" alt="" class="img-galeria">
+                   <img src="imagen12.jpg" alt="" class="img-galeria">
+               </div>
+            </div>
+        </section>
+        <section class="imagen-light">
+        <img src="close.png" alt="" class="close">
+        <img src="" alt="" class="agregar-imagen">
+        </section>
+        <section class="contenedor" id="registrese">
+            <h2 class="subtitulo">Hay muchas maneras de ayudar </h2>
+            <section class="experts">
+<div class="contenedor-expert">
+    <img src="perostik.png" alt="">
+    <h3 class="n-experts"> Adoptar</h3>
+</div>
+<div class="contenedor-expert">
+    <img src="gatostik.png" alt="">
+    <h3 class="n-experts"> Dar en adopcion</h3>
+</div>
+<div class="contenedor-expert">
+    <img src="conejostik.png" alt="">
+    <h3 class="n-experts"> Ver mas</h3>
+</div>
+
+
+            </section>
+
+            <a href="index1.php"> Registrarse</a>
+
+        </section>
+    </main>
+
+    <footer id="contactos">
+       <div class="contenedor-footer">
+           <div class="content-foo">
+            <h4> Direccion</h4>
+            <p>Mulalo, Latacunga</p>
+            <p>Lotizacion Padre Guillermo Rivera</p>
+           </div>
+           <div class="content-foo">
+            <h4> Correo</h4>
+            <p>adoptauna@gmail.com</p>
+            <p>mascotasayuda@.hotmail.com</p>
+           </div>
+           <div class="content-foo">
+            <h4> Telefono</h4>
+            <p>(03)-2808723</p>
+            <p>0984844710</p>
+           </div>
+       </div>
+
+    </footer>
+    <script src="menu.js"></script>
+    <script src="lightbox.js"></script>
+
+</body>
+</html>
